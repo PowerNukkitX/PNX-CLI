61ed71c32ad3b005ae118a2862cda3b6902c21b3
CoolLoong <1542536763@qq.com>
2022-11-05 10:05:43 +0800

    更新0.0.7版本

--- src/main/java/cn/powernukkitx/cli/share/CLIConstant.java
+++ src/main/java/cn/powernukkitx/cli/share/CLIConstant.java
@@ -4,10 +4,9 @@ import cn.powernukkitx.cli.util.OSUtils;
 
 import java.io.File;
 import java.util.List;
-import java.util.Timer;
 
 public interface CLIConstant {
-    String version = "0.0.6";
+    String version = "0.0.7";
     List<String> authors = List.of("超神的冰凉");
     File userDir = new File(System.getProperty("user.dir"));
     File programDir = new File(OSUtils.getProgramDir());
